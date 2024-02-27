5255e5e271f48b32807be528565c8e3009adccc6
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-27 08:44:00 +0200

    Initial Commit

---
+++ HowToUseControlGroupInSwiftUI/HowToUseControlGroupInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseControlGroupInSwiftUIApp.swift
+//  HowToUseControlGroupInSwiftUI
+//
+//  Created by test on 27.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseControlGroupInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
